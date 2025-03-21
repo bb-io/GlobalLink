38417763efa1e4d92c4a0f32260fa071087fe741
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-03-21 17:47:46 +0200

    test(PhaseNameDataHandler): 🔧 Update test class to use specific `SubmissionId` and defined `SearchString`; set `CanBeEmpty` to true for improved test clarity

--- Tests.GlobalLink/DataHandlerTests/PhaseNameDataHandlerTests.cs
+++ Tests.GlobalLink/DataHandlerTests/PhaseNameDataHandlerTests.cs
@@ -1,12 +1,15 @@
-using System;
 using Apps.GlobalLink.Handlers;
 using Blackbird.Applications.Sdk.Common.Dynamic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests.GlobalLink.DataHandlerTests;
 
+[TestClass]
 public class PhaseNameDataHandlerTests : BaseDataHandlerTests
 {
-    protected override IAsyncDataSourceItemHandler DataHandler => new PhaseNameDataHandler(InvocationContext, new() { SubmissionId = ""});
+    protected override IAsyncDataSourceItemHandler DataHandler => new PhaseNameDataHandler(InvocationContext, new() { SubmissionId = "17908"});
 
-    protected override string SearchString => throw new NotImplementedException();
+    protected override string SearchString => "Translation";
+
+    protected override bool CanBeEmpty => true;
 }
