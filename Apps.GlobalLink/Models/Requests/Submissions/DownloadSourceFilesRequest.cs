68f1d1b9b84a0871f23721450eaafa905303a438
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-04-10 13:07:23 +0300

    fix(submission): 🔧 Reorder properties in `DownloadSourceFilesRequest` class;

--- Apps.GlobalLink/Models/Requests/Submissions/DownloadSourceFilesRequest.cs
+++ Apps.GlobalLink/Models/Requests/Submissions/DownloadSourceFilesRequest.cs
@@ -5,10 +5,10 @@ using Blackbird.Applications.Sdk.Common.Dynamic;
 namespace Apps.GlobalLink.Models.Requests.Submissions;
 
 public class DownloadSourceFilesRequest
-{
-    [Display("Phase name"), DataSource(typeof(PhaseNameDataHandler))]
-    public string PhaseName { get; set; } = string.Empty;
-
+{   
     [Display("Submission ID"), DataSource(typeof(SubmissionDataHandler))]
     public string SubmissionId { get; set; } = string.Empty;
+
+    [Display("Phase name"), DataSource(typeof(PhaseNameDataHandler))]
+    public string PhaseName { get; set; } = string.Empty;
 }
